ac2ae8a229c184276f71e61305780e055267723e
Gabriel Edgar <94152526+nois44@users.noreply.github.com>
2022-11-24 10:37:16 +0700

    for flutter assignment

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from mywatchlist.views import show_watch_list, show_xml, show_json
+from mywatchlist.views import show_watch_list, show_xml, show_json, show_json-by_id
 
 app_name = 'mywatchlist'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', show_watch_list, name='show_watch_list'),
     path('html/', show_watch_list, name='show_watch_list'),
     path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json')
+    path('json/', show_json, name='show_json'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
 ]
